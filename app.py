32cb94b9eec82c680d4f24455f2c49419854a51b
Carlos Rodriguez <carlos.rodriguezacosta@gmail.com>
2024-12-13 13:03:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -217,4 +217,4 @@ def get_conversations():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
